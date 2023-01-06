3b4548587b26b5d15a52b672a62a62c797a89625
alankarapte <alankarapte7588@gmail.com>
2023-01-07 02:08:12 +0530

    network first approach

--- sw.js
+++ sw.js
@@ -41,20 +41,51 @@ self.addEventListener('activate', (event) => {
 });
 
 
+// CODE: always-hit-cache
+// self.addEventListener('fetch', (event) => {
+//     console.log('funcational event: fetch');
+//     console.log('TEST: ', event.request);
+
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then(response => {
+//                 // cache hit ~ return response
+//                 if (response) {
+//                     return response;
+//                 }
+//                 return fetch(event.request)
+//                     //fallback to cache anyway
+//                     .catch(() => caches.match(event.request));
+//             })
+//     );
+// });
+
+/**
+ * Issue in above approach (CODE: always-hit-cache - commented above):
+ * we are telling our service worker to respond always from cache storage.
+ * but what if we update any file on server e.g. added new content in HTML/added new CSS styles etc.
+ * even though we never gonnna hit the server as we always respnding through cache.
+ * 
+ * One Solution (CODE: always-hit-network-first-else-cache):
+ * we are telling our service worker to respond from server.
+ * but when offline respond from cache instead.
+ */
+
+// CODE: always-hit-network-first-else-cache
 self.addEventListener('fetch', (event) => {
     console.log('funcational event: fetch');
     console.log('TEST: ', event.request);
 
     event.respondWith(
-        caches.match(event.request)
+        fetch(event.request)
             .then(response => {
-                // cache hit ~ return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
-                    //fallback to cache anyway
-                    .catch(() => caches.match(event.request));
+                console.log('caching response.');
+                const cacheResponse = response.clone();
+                caches.open(cacheKey)
+                    .then(cache => cache.put(event.request, cacheResponse));
+                return response;
             })
+            //fallback to cache
+            .catch(() => caches.match(event.request))
     );
 });
